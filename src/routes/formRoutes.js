4c0b78cf6d1512009a6e33bb97dcfc41b2e6c407
belouse59 <121089456+belouse59@users.noreply.github.com>
2026-05-03 01:27:00 +0200

    Delete src/routes/formRoutes.js

--- src/routes/formRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { submitForm } = require("../controllers/formController");
-
-router.post("/submit", submitForm);
-
-module.exports = router;
