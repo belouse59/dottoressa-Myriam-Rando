147d07501f2a16fdf1228564302882f69401cf4e
belouse59 <121089456+belouse59@users.noreply.github.com>
2026-05-03 21:29:15 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -35,8 +35,7 @@ app.use((err, req, res, next) => {
 // health check
 app.get("/health", (req, res) => {
     res.json({
-        status: "ok",
-        project: "mmconsultingagency 🚀"
+        status: "ok"
     });
 });
 
